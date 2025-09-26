c21638a4e3c7024c3718e54231ed2f67c67fc992
nclet <39292336+nclet@users.noreply.github.com>
2025-09-26 14:09:56 +0900

    Update 14_테스트.py

--- pages/14_테스트.py
+++ pages/14_테스트.py
@@ -225,7 +225,7 @@ def calculate_and_add_indicators(df, show_ma, show_bb, show_rsi):
         df['BB_Lower'] = df['MA20'] - (df['STD20'] * 2)
         
         apds.append(mpf.make_addplot(df['BB_Upper'], color='purple', linestyle=':', panel=0, label='BB_Upper'))
-        apds.append(mpf.make_addplot(df['BB_Lower'], color='purple', linestyle=':', panel=0, label='BB_Lower'))
+        apds.append(mpf.make_addplot(df['BB_Lower'], color='#FFB6C1', linestyle=':', panel=0, label='BB_Lower'))
 
     # RSI (상대강도지수) 계산 및 추가 (14일 기준)
     if show_rsi:
